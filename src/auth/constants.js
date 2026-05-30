9ef6354ae21298a3f68754fbb2df3d3b0b1fd2ae
asuma-labs <asuma.my.id@gmail.com>
2026-05-30 18:03:10 +0700

    Create constants.js

---
+++ src/auth/constants.js
@@ -0,0 +1,9 @@
+export const ALGORITHM = 'aes-256-gcm';
+export const IV_LENGTH = 16;
+export const TAG_LENGTH = 16;
+
+export const SHARED_KEY_TYPES = new Set([
+    'lid-mapping',
+    'device-list',
+    'tctoken',
+]);
